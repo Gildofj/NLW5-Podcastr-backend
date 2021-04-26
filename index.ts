242988734f2af536968fed32df6faa613e57dfb9
Gildo Junior <1gildojunior@gmail.com>
2021-04-26 00:21:52 -0300

    vercel config

--- index.ts
+++ index.ts
@@ -8,7 +8,10 @@ dotenv.config();
 
 // Connect to MongoDB database
 mongoose
-  .connect(process.env.MONGO_URI ?? "", { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI ?? "", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     const app = express();
     app.use("/api", episodes);
